f97894fc8c5595e85627043e5e1524f60fdea0b8
Tim Sawicki <tim@quix.io>
2025-08-21 15:39:27 -0400

    more print

--- iceberg-rest-catalog/postgres_catalog_service.py
+++ iceberg-rest-catalog/postgres_catalog_service.py
@@ -22,7 +22,6 @@ class PostgresCatalogService:
     def __init__(self):
         # Database configuration
         self.db_host = os.environ['POSTGRES_HOST'],
-        print(self.db_host)
         self.db_port = int(os.getenv('POSTGRES_PORT', '5432'))
         self.db_name = os.getenv('POSTGRES_DB', 'iceberg_catalog')
         self.db_user = os.getenv('POSTGRES_USER', 'postgres')
@@ -88,6 +87,12 @@ class PostgresCatalogService:
         conn = None
         try:
             # Connect to the default 'postgres' database to create our target database
+            print(
+                {
+                    'HOST': self.db_host,
+                    'PORT': self.db_port,
+                }
+            )
             conn = psycopg2.connect(
                 host=self.db_host,
                 port=self.db_port,
